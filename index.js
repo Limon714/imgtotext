5ff8f23ef8355b854e5c55d62f0aab5d6cd50c02
Limon <limon4u714@gmail.com>
2022-11-20 13:14:10 +0600

    use mongodb localhost to port number

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+// database connection with mongoose
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+.then(()=> console.log("Connection Successful"))
+.catch((err) => console.log(err));
+
+// function errorHandling(err, req, res, next){
+//     if(res.headersSent){
+//         return next(err);
+//     }
+
+//     res.status(500).json({error: err})
+// }
+
+app.listen(3000, ()=>{
+
+    console.log("Server run at port 3000");
+
+});
+
+app.get('/', (req,res)=>{
+
+    res.send("Welcome to Homepage");
+
+})
